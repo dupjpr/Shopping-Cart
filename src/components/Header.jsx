5187bd223867212f85a05e8a68c4d2d30f2bae13
pakman <pakman@DESKTOP-J81E3KF.localdomain>
2021-01-31 22:14:58 -0500

    added the animation to the logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,16 @@ const Header = ({ handleClickBag, cart, total }) => {
     return (
         <header >
             <div className="header-logo">
-                <span><i className="fas fa-ship"></i></span>
+                <div className="logoA">
+                    <span><i className="fas fa-ship"></i></span>
+                    <div class='uno'></div>
+                    <div class='dos'></div>
+                    <div class='tres'></div>
+                </div>
                 <h1>Sea Food</h1>
             </div>
             <div className="header-bag">
-               <i className="fas fa-shopping-bag" onClick={(e) => handleClickBag(e)}></i>
+                <i className="fas fa-shopping-bag" onClick={(e) => handleClickBag(e)}></i>
                 <span className='total'>${total}</span>
                 <p className='number-items'>{cart.length}</p>
             </div>
